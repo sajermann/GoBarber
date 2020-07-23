eb308bf2cb9cd4397d593bd616a5b03a824af163
Bruno Sajermann <contato@sajermann.com>
2020-07-22 22:48:02 -0300

    Video 1/2/5/11 Complete

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({message: 'Hello Wolrd'})
-})
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
 
 export default routes;
