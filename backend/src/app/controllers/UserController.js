14a1fee0f6ed145ca974a9d266b5cd8b4e9fb130
Bruno Sajermann <contato@sajermann.com>
2020-07-24 08:13:01 -0300

    Video 1/2/5/15 Complete

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -17,6 +17,11 @@ class UserController {
     });
   }
 
+  async update(req, res) {
+    console.log(req.userId);
+    return res.json({ ok: true });
+  }
+
   async index(req, res) {
     const users = await User.findAll();
 
